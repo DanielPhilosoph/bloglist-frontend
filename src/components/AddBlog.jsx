1ca8e8df5db6f16e035dabc9dcd16bd4b23b5d35
DanielPhilosoph <danielph9@gmail.com>
2021-12-14 18:25:01 +0200

    5.6

---
+++ src/components/AddBlog.jsx
@@ -0,0 +1,86 @@
+import axios from "axios";
+import { useRef } from "react/cjs/react.development";
+import Swal from "sweetalert2";
+
+const AddBlog = (props) => {
+  const title = useRef();
+  const author = useRef();
+  const url = useRef();
+
+  const newBlogClick = async () => {
+    try {
+      if (title.current.value && author.current.value && url.current.value) {
+        const token =
+          "bearer " + JSON.parse(localStorage.getItem("user")).token;
+        const response = await axios.post(
+          "/api/blogs",
+          {
+            title: title.current.value,
+            author: author.current.value,
+            url: url.current.value,
+          },
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        );
+
+        // Update blogs & close new blog form
+        props.setBlogs(response.data);
+        props.setShowForm(false);
+
+        // Display massage
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `Added blog ${title.current.value}!`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+
+        // Reset fields
+        title.current.value = "";
+        author.current.value = "";
+        url.current.value = "";
+      } else {
+        Swal.fire({
+          position: "top-end",
+          icon: "error",
+          title: "Missing field",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      Swal.fire({
+        position: "top-end",
+        icon: "error",
+        title: error.response,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
+
+  return (
+    <form style={{ display: props.showForm ? "block" : "none" }}>
+      <span>Title: </span>
+      <input type="text" placeholder="Title" ref={title}></input>
+      <br />
+      <span>Author: </span>
+      <input type="text" placeholder="Author" ref={author}></input>
+      <br />
+      <span>URL: </span>
+      <input type="text" placeholder="URL" ref={url}></input>
+      <br />
+      <br />
+      <button type="button" onClick={newBlogClick}>
+        create
+      </button>
+    </form>
+  );
+};
+
+export default AddBlog;
--- src/components/BlogsList.jsx
+++ src/components/BlogsList.jsx
@@ -2,19 +2,12 @@ import React, { useState, useEffect } from "react";
 import Blog from "./Blog";
 import getAll from "../services/blogs";
 import { useNavigate } from "react-router-dom";
-import { useRef } from "react/cjs/react.development";
-import axios from "axios";
+import AddBlog from "./AddBlog";
 
 const BlogsList = (props) => {
   const [blogs, setBlogs] = useState([]);
   const navigate = useNavigate();
-  const [showMessage, setShowMessage] = useState(false);
   const [showForm, setShowForm] = useState(false);
-
-  const title = useRef();
-  const author = useRef();
-  const url = useRef();
-  const massage = useRef();
 
   useEffect(() => {
     getAll(props.token).then((blogs) => setBlogs(blogs));
@@ -25,51 +18,6 @@ const BlogsList = (props) => {
     navigate("/");
   };
 
-  const newBlogClick = async () => {
-    try {
-      if (title.current.value && author.current.value && url.current.value) {
-        const token =
-          "bearer " + JSON.parse(localStorage.getItem("user")).token;
-        const response = await axios.post(
-          "/api/blogs",
-          {
-            title: title.current.value,
-            author: author.current.value,
-            url: url.current.value,
-          },
-          {
-            headers: {
-              Authorization: token,
-            },
-          }
-        );
-
-        // Update blogs & close new blog form
-        setBlogs(response.data);
-        setShowForm(false);
-
-        // Display massage
-        setShowMessage(true);
-        massage.current.innerText = `Added blog ${title.current.value}!`;
-
-        // Reset fields
-        title.current.value = "";
-        author.current.value = "";
-        url.current.value = "";
-
-        // Remove massage
-        setTimeout(() => {
-          massage.current.innerText = "";
-          setShowMessage(false);
-        }, 5000);
-      } else {
-        console.log("missing field");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   const toggleForm = () => {
     setShowForm(showForm ? false : true);
   };
@@ -78,11 +26,6 @@ const BlogsList = (props) => {
     <div className="container">
       <h1>blogs</h1>
       <h3>{props.username} has logged in</h3>
-      <p
-        ref={massage}
-        className="massage"
-        style={{ display: showMessage ? "block" : "none" }}
-      ></p>
       <p>
         <button onClick={logout}>logout</button>
       </p>
@@ -96,21 +39,11 @@ const BlogsList = (props) => {
       <button onClick={toggleForm}>Create new blog</button>
       <br />
       <br />
-      <form style={{ display: showForm ? "block" : "none" }}>
-        <span>Title: </span>
-        <input type="text" placeholder="Title" ref={title}></input>
-        <br />
-        <span>Author: </span>
-        <input type="text" placeholder="Author" ref={author}></input>
-        <br />
-        <span>URL: </span>
-        <input type="text" placeholder="URL" ref={url}></input>
-        <br />
-        <br />
-        <button type="button" onClick={newBlogClick}>
-          create
-        </button>
-      </form>
+      <AddBlog
+        showForm={showForm}
+        setShowForm={setShowForm}
+        setBlogs={setBlogs}
+      />
     </div>
   );
 };
