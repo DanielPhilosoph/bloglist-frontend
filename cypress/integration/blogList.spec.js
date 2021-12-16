92508820bf65129685902898070e4165270c582c
DanielPhilosoph <danielph9@gmail.com>
2021-12-16 22:50:39 +0200

    done

--- cypress/integration/blogList.spec.js
+++ cypress/integration/blogList.spec.js
@@ -11,23 +11,23 @@ describe("Blog app", function () {
   });
 
   describe("login form", function () {
-    // it("should show login form by default", function () {
-    //   cy.get("#username").should("exist");
-    //   cy.get("#password").should("exist");
-    //   cy.get("#login").should("exist");
-    // });
+    it("should show login form by default", function () {
+      cy.get("#username").should("exist");
+      cy.get("#password").should("exist");
+      cy.get("#login").should("exist");
+    });
 
-    // it("front page can be opened", function () {
-    //   cy.contains("Login to blog list app!");
-    // });
+    it("front page can be opened", function () {
+      cy.contains("Login to blog list app!");
+    });
 
-    // it("will show error if password is wrong", function () {
-    //   cy.get("#username").type("edan");
-    //   cy.get("#password").type("not_the_right_password");
-    //   cy.get("#login").click();
-    //   cy.get(".swal2-shown").contains("invalid username or password");
-    //   cy.get("html").should("not.contain", "edan has logged in");
-    // });
+    it("will show error if password is wrong", function () {
+      cy.get("#username").type("edan");
+      cy.get("#password").type("not_the_right_password");
+      cy.get("#login").click();
+      cy.get(".swal2-shown").contains("invalid username or password");
+      cy.get("html").should("not.contain", "edan has logged in");
+    });
 
     it("can login to the app", function () {
       cy.get("#username").type("edan");
@@ -52,48 +52,48 @@ describe("Blog app", function () {
       cy.visit("http://localhost:3000/blogs");
     });
 
-    // it("A blog can be created", function () {
-    //   cy.get("#toggleCreateFrom").should("exist");
+    it("A blog can be created", function () {
+      cy.get("#toggleCreateFrom").should("exist");
 
-    //   cy.get("#toggleCreateFrom").click();
-    //   cy.get("#createTitle").type("Im a new title");
-    //   cy.get("#createAuthor").type("Author");
-    //   cy.get("#createUrl").type("some.url.com");
-    //   cy.get(".createButton").click();
+      cy.get("#toggleCreateFrom").click();
+      cy.get("#createTitle").type("Im a new title");
+      cy.get("#createAuthor").type("Author");
+      cy.get("#createUrl").type("some.url.com");
+      cy.get(".createButton").click();
 
-    //   cy.get(".blog > .titleSpan").contains("Im a new title");
+      cy.get(".blog > .titleSpan").contains("Im a new title");
 
-    //   cy.visit("http://localhost:3000");
-    // });
+      cy.visit("http://localhost:3000");
+    });
 
-    // it("Can like a blog", function () {
-    //   cy.get("#toggleCreateFrom").click();
-    //   cy.get("#createTitle").type("Im a new title");
-    //   cy.get("#createAuthor").type("Author");
-    //   cy.get("#createUrl").type("some.url.com");
-    //   cy.get(".createButton").click();
+    it("Can like a blog", function () {
+      cy.get("#toggleCreateFrom").click();
+      cy.get("#createTitle").type("Im a new title");
+      cy.get("#createAuthor").type("Author");
+      cy.get("#createUrl").type("some.url.com");
+      cy.get(".createButton").click();
 
-    //   cy.get(".blog > .showBtn").click();
-    //   cy.get(".likeButton").click();
+      cy.get(".blog > .showBtn").click();
+      cy.get(".likeButton").click();
 
-    //   cy.get(".likes").contains("Likes: 1");
+      cy.get(".likes").contains("Likes: 1");
 
-    //   cy.visit("http://localhost:3000");
-    // });
+      cy.visit("http://localhost:3000");
+    });
 
-    // it("Can delete a blog", function () {
-    //   cy.get("#toggleCreateFrom").click();
-    //   cy.get("#createTitle").type("Im a new title");
-    //   cy.get("#createAuthor").type("Author");
-    //   cy.get("#createUrl").type("some.url.com");
-    //   cy.get(".createButton").click();
+    it("Can delete a blog", function () {
+      cy.get("#toggleCreateFrom").click();
+      cy.get("#createTitle").type("Im a new title");
+      cy.get("#createAuthor").type("Author");
+      cy.get("#createUrl").type("some.url.com");
+      cy.get(".createButton").click();
 
-    //   cy.get(".blog > .showBtn").click();
-    //   cy.get(".removeBtn").click();
+      cy.get(".blog > .showBtn").click();
+      cy.get(".removeBtn").click();
 
-    //   cy.get("blog").should("not.exist");
-    //   cy.visit("http://localhost:3000");
-    // });
+      cy.get("blog").should("not.exist");
+      cy.visit("http://localhost:3000");
+    });
 
     it("Check blog order by likes", function () {
       // Adding first blog
